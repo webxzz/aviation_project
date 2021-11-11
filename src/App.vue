40cde4367da9d57daa95cee3c66a769fff839fda
first <2145920341@qq.com>
2021-11-11 11:44:45 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="app">
+       <router-view></router-view>
+   </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+body,html,#app{
+  height: 100%;
+}
+body{
+  font-size: 0.14rem;
+  background: #f5f7f9;
+}
+</style>
